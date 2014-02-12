d017512231fc01ac88427b5a772d61a09d15a506
breezedu <breeze.du@gmail.com>
2014-02-12 09:35:57 -0500

    update BestTimetoBuyandSellStock III class

--- BestTimeToBuyAndSellStockIII.java
+++ BestTimeToBuyAndSellStockIII.java
@@ -3,10 +3,13 @@ package leetCode2012;
 import java.util.Scanner;
 
 /***********
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * If you were only permitted to complete at most one transaction 
- * (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
- * 
+ * Say you have an array for which the ith element is 
+ * the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. 
+ * You may complete at most two transactions.
+ * Note:
+ * You may not engage in multiple transactions at the same time 
+ * (ie, you must sell the stock before you buy again).
  * @author Frog
  *
  */
@@ -51,21 +54,22 @@ public class BestTimeToBuyAndSellStockIII {
 			return 0;
 		} // end if condition;
 		
-        int minLR = prices[0];
-        int maxRL = prices[prices.length - 1];
-        
-        int[] dpLR = new int[prices.length];
-        int[] dpRL = new int[prices.length];
-        int max = 0;
-        
-        for(int i = 1; i < prices.length; i++){
-            if(prices[i] < minLR) 
-            	minLR = prices[i];
-            
-            dpLR[i] = Math.max(dpLR[i - 1], prices[i] - minLR);
-        }
-        
-        for(int i = prices.length - 2; i > -1; i--){
+		int minLR = prices[0];
+		int maxRL = prices[prices.length - 1];
+		
+		int[] dpLR = new int[prices.length];
+		int[] dpRL = new int[prices.length];
+		
+		int max = 0;
+		
+		for(int i = 1; i < prices.length; i++){
+			if(prices[i] < minLR)
+				minLR = prices[i];
+			
+			dpLR[i] = Math.max(dpLR[i - 1], prices[i] - minLR);
+		} // end for i<price.length loop;
+		
+		for(int i = prices.length - 2; i > -1; i--){
             if(prices[i] > maxRL) 
             	maxRL = prices[i];
             
