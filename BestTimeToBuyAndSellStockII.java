d4e78891649484bd580d39b0a0a1995a00bc9c8d
breezedu <breeze.du@gmail.com>
2014-02-12 10:02:47 -0500

    update BestTimetoBuyandSellStock II class

--- BestTimeToBuyAndSellStockII.java
+++ BestTimeToBuyAndSellStockII.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 /***********
  * Say you have an array for which the ith element is the price of a given stock on day i.
- * If you were only permitted to complete at most one transaction 
- * (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * Design an algorithm to find the maximum profit. 
+ * You may complete as many transactions as you like 
+ * (ie, buy one and sell one share of the stock multiple times). 
+ * However, you may not engage in multiple transactions at the same time 
+ * (ie, you must sell the stock before you buy again).
  * 
  * @author Frog
  *
@@ -44,6 +47,11 @@ public class BestTimeToBuyAndSellStockII {
 		return prices;
 	} // end buildArray() loop;
 
+	/*********
+	 * just calculate everyday 'rise";
+	 * @param prices
+	 * @return
+	 */
 	private static int maxProfit(int[] prices) {
 		// TODO To calculate the max profit
 		int Len = prices.length;
@@ -51,7 +59,7 @@ public class BestTimeToBuyAndSellStockII {
 			return 0;
 		}
 		
-		int max = 0; // this is the worst condition, buy and sell the stock at the same day;
+		int max = 0; // this is the worst condition, no profit;
 		for(int i=0; i<Len-1; i++){
 			
 			if(prices[i+1]>prices[i]){
