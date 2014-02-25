3240570e7dcfd1245220b62fa172baa3abacc5b0
breezedu <breeze.du@gmail.com>
2014-02-25 12:09:38 -0500

    add Interval.java

---
+++ Interval.java
@@ -0,0 +1,13 @@
+package leetCode2012;
+
+public class Interval {
+
+	    int start;
+	    int end;
+	    Interval() { start = 0; end = 0; }
+	      
+	    Interval(int s, int e) { 
+	    	start = s; end = e; 
+	    }
+	 
+}
