bca3f79cfe81de2065418be6577019f2d119754f
breezedu <breeze.du@gmail.com>
2014-02-25 12:11:32 -0500

    update SubstetsII.java class

--- SubsetsII.java
+++ SubsetsII.java
@@ -10,16 +10,14 @@ import java.util.Scanner;
  * Elements in a subset must be in non-descending order.
  * The solution set must not contain duplicate subsets.
  * For example,
- * If S = [1,2,3], a solution is:
+ * If S = [1,2,2], a solution is:
  * 
  * [
- *   [3],
  *   [1],
  *   [2],
- *   [1,2,3],
- *   [1,3],
- *   [2,3],
+ *   [1,2,2],
  *   [1,2],
+ *   [2,2],
  *   []
  *   ]
  *   
