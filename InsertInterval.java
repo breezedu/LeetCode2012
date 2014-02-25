52ac11ea7fd5554e3425100796ba8be5220db97f
breezedu <breeze.du@gmail.com>
2014-02-25 13:46:43 -0500

    add InsertInterval.java

---
+++ InsertInterval.java
@@ -0,0 +1,141 @@
+package leetCode2012;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*******************
+ * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+ * You may assume that the intervals were initially sorted according to their start times.
+ * Example 1:
+ * Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+ * Example 2:
+ * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+ * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+ * 
+ * @author Frog
+ *
+ */
+public class InsertInterval {
+	
+	public static void main(String[] args){
+		
+		System.out.println("This is Insert Interval program.");
+		
+		//1st, create N Intervals, put them into an arrayList
+		ArrayList<Interval> IntervalAL = createIntervals();
+		printALofIntervals(IntervalAL);
+		
+		
+		//2nd, create another Interval
+		Interval newInt = new Interval();
+		newInt.start = (int)(Math.random()*25);
+		newInt.end = newInt.end + (int)(Math.random()*50);
+		System.out.println("The new Interval is: ["+newInt.start +", "+newInt.end +"] ");
+		
+		
+		//3rd, insert the solo Interval into the arrayList
+		IntervalAL = insertInterval(IntervalAL, newInt);
+		
+		
+		//4th, printout the final Intervals arrayList
+		printALofIntervals(IntervalAL);
+		
+	}//end main();
+
+	private static ArrayList<Interval> insertInterval(ArrayList<Interval> intervalAL, Interval newInt) {
+		// TODO Insert an Interval into an arrayList of Intervals, merge all overlaps
+		
+		//if the AL is empty, or newInt.start is bigger than the last Interval.end in the AL:
+		if(intervalAL==null || intervalAL.size()==0 || newInt.start>intervalAL.get(intervalAL.size()-1).end){
+			intervalAL.add(newInt);
+			return intervalAL;
+		}
+		
+		ArrayList<Interval> retAL = new ArrayList<Interval>();
+		if(newInt.end < intervalAL.get(0).start){
+			
+			retAL.add(newInt);
+			retAL.addAll(intervalAL);
+			return retAL;
+		}//end if condition;
+		
+		int Len = intervalAL.size();
+		for(int i=0; i<Len; i++){
+
+			Interval temp = intervalAL.get(i);
+			if(temp.end < newInt.start){
+				retAL.add(temp);
+				
+			} else if(temp.start > newInt.end){
+				
+				retAL.add(newInt);
+				newInt = temp;
+				
+			} else {
+				
+				newInt = merge2Intervals(newInt, temp);
+			}//end if-else conditions;
+			
+		}//end for loop;
+		
+		retAL.add(newInt);
+		
+		return retAL;
+	}//end insertInterval() method;
+
+	private static Interval merge2Intervals(Interval Int1, Interval Int2) {
+		// TODO merge two Intervals
+		
+		Int1.start = Math.min(Int1.start, Int2.start);
+		Int1.end = Math.max(Int1.end, Int2.end);
+		
+		return Int1;
+	}//end merge2Intervals() method;
+
+	private static ArrayList<Interval> createIntervals() {
+		// TODO Create Intervals, and put them into an arrayList
+		ArrayList<Interval> retAL = new ArrayList<Interval>();
+		
+		System.out.println("Please input the number of Intervals:");
+		Scanner input = new Scanner(System.in);
+		System.out.print("num = ");
+		int num = input.nextInt();
+		input.close();
+		
+		if(num==0){
+			System.out.println("No Interval will be created, the AL will be empty.");
+			return retAL;
+		}
+		
+		int start = (int)(Math.random() * 10);
+		
+		for(int i=0; i<num; i++){
+			Interval temp = new Interval();
+			temp.start = start + (int)(Math.random()*10);
+			temp.end = temp.start + (int)(Math.random()*10);
+			
+			retAL.add(temp); //add the temp Interval to arrayList;
+			
+			start = temp.end+1; //reset the start value;
+			
+		}//end for i<num loop;
+		
+		return retAL;
+	}//end createIntervals() method;
+
+	private static void printALofIntervals(ArrayList<Interval> interAL) {
+		// TODO printout all Intervals in an arrayList
+		if(interAL==null || interAL.isEmpty()){
+			System.out.println("There's no Interval in the arrayList.");
+			return;
+		}//end if interAL is empty condition;
+		
+		for(Interval e:interAL){
+			
+			System.out.print(" [" + e.start +", " + e.end +"]");
+		}//end for e loop;
+		
+		System.out.println();
+	}//end printALofIntervals() method
+
+}//end of everything in InsertInterval class;
