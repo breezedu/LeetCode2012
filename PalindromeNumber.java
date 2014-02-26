0e174fde2ac4c7d6469caa7422cce06e0009403e
breezedu <breeze.du@gmail.com>
2014-02-26 12:32:54 -0500

    update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -20,21 +20,26 @@ public class PalindromeNumber {
 		int num = input.nextInt();
 		input.close();
 		
+		
+		//2nd, check if the inputed integer is palindrome or not;
 		boolean palin = isPalindrome(num);
 		
 		if(palin){
 			System.out.println("Yes, " + num +" is a Palindrome number.");
+			
 		} else {
 			System.out.println("No, that's not a palindrome number.");
-		}
+			
+		}//end if-else conditions;
 		
 	}//end main();
 
 	private static boolean isPalindrome(int num) {
 		// TODO check if a number is palindrome;
 		if(num <0) return false;
-		if(num <10) return true;
+		if(num <10) return true; //0-9 are palindromes;
 		
+		//diff means the 'levels' of num: 10 for 10-99, 100 for 100-999.... 
 		int diff = 10;
 		while(num/diff >= 10){
 			diff *= 10;
@@ -43,20 +48,21 @@ public class PalindromeNumber {
 		
 		return checkPalindrome(num, diff);
 		
-	}
+	}//end isPalindrome() method;
 
 	private static boolean checkPalindrome(int num, int diff) {
 		// TODO check if a num of diff levels is palindrome or not;
-		if(diff==0){
+		if(diff==0 || diff==1){
 			return true;
-		}
+		}//end directly return conditions;
+		
 		System.out.println("num= " + num +" num/diff=" + num/diff +", num%10=" + num%10);
 		if(num/diff != num%10) return false;
 		
 		num = num%diff;
-		num = num/10;
+		num = num/10;		
+		return checkPalindrome(num, diff/100); //recursion
 		
-		return checkPalindrome(num, diff/100);
-	}
+	}//end checkPalindrome() method;
 	
 }//end of everything in PalindromeNumber class;
