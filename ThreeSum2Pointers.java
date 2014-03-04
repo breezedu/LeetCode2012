3fc49ad6e8ffbb5251b385f2e65df9bfcca1fd85
breezedu <breeze.du@gmail.com>
2014-03-03 23:57:12 -0500

    update ThreeSum2Pointers.java

--- ThreeSum2Pointers.java
+++ ThreeSum2Pointers.java
@@ -2,6 +2,7 @@ package leetCode2012;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /******************
  * Given an array S of n integers, are there elements a, b, c in S 
@@ -23,15 +24,19 @@ public class ThreeSum2Pointers {
 	public static void main(String[] args){
 		
 		System.out.println("This is Three Sum program.");
-		int[] array = {79,-82,-37,-93,57,-6,-94,38,94,88,-60,-22,77,-44,-38,0,99,-57,-69,-78,
-				85,78,-3,-5,89,-11,93,42,-9,-74,90,96,54,-91,34,-14,52,96,5,18,93,76,-95,-86,
-				36,-83,65,-80,-63,74,4,-82,12,-78,11,-78,-57,-21,37,87,-32,-64,79,-14,46,-72,
-				75,-6,-63,-71,11,-10,68,85,-3,-9,-72,31,-83,-24,32,45,-82,26,-67,85,10,-78,-92,
-				-14,65,-63,-63,-53,38,-6,-5,42,-35,97,-30,-12,-7,-51,85,26,21,52,48,-62,48,21,
-				86,5,64,56,49,-67,-91,-21,16,25,-1,54,48,0,49,81,16,89,53,40,-44,-58,-91,77,58,
-				77,-10,-74,-11,86,27,27,-84,-23,92,38,35,-3,-6,-99,47,55,71,-82,-34,8,32,-4,17,-52};
-		//int target = -141;
 		
+		//1st, build an array
+		System.out.println("Please input the num of elements in the array.");
+		Scanner input = new Scanner(System.in);
+		System.out.print("num = ");
+		int num = input.nextInt();
+		input.close();
+		
+		int[] array = createArray(num);
+		printArray(array);
+		
+		
+		//2nd, static 3-sum to 0;		
 		ArrayList<ArrayList<Integer>> ALofAL = threeSum(array);
 		
 		printALofAL(ALofAL);
@@ -100,6 +105,32 @@ public class ThreeSum2Pointers {
 		}
 		System.out.println();
 	}
+	
+	private static int[] createArray(int num){
+		if(num == 0) return null;
+		int[] array = new int[num];
+		
+		for(int i=0; i<num; i++){
+			array[i] = (int)(Math.random() * 15);
+		}
+		
+		return array;
+	}//end createArray() method;
+	
+	private static void printArray(int[] array){
+		if(array == null){
+			System.out.println("it's an empty array.");
+			return;
+		}
+		
+		int Len = array.length;
+		for(int i=0; i<Len; i++){
+			
+			System.out.print(" " +array[i]);
+		}
+		
+		System.out.println();
+	}//end printArray() method;
 
 	private static void printArrayList(ArrayList<Integer> al) {
 		// TODO printout arrayList
