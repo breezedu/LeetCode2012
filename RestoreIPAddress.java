91d0ad5582536b6f5e4497066204b50c5af65405
breezedu <breeze.du@gmail.com>
2014-03-09 17:41:04 -0400

    update RestoreIPAddress.java class

--- RestoreIPAddress.java
+++ RestoreIPAddress.java
@@ -36,6 +36,12 @@ public class RestoreIPAddress {
 		
 	}//end main()
 
+	/**********
+	 * divide the ip-address into 4 sections; check step by step for each section;
+	 * call checkIpSections() method to add another valid ip section to the currentIP string;
+	 * @param ip
+	 * @return
+	 */
 	private static ArrayList<String> restoreIpAddresses(String ip) {
 		// TODO Auto-generated method stub
 		ArrayList<String> ipSets = new ArrayList<String>();
@@ -48,16 +54,29 @@ public class RestoreIPAddress {
 		return ipSets;
 	}//end restoreIpAddresses() method;
 
+	/***********
+	 * if count==1, which means checking for the very last ip-section;
+	 * if the 'original' ip-string left is a valid ip-section, add it to the currentIP string;
+	 * and add the currentIP string to the ipSets arrayList;
+	 * otherwise, if the count>1, check if the original ip string's length is valid (<count*3)
+	 * take 1, 2 or 3 characters from the original ip string, if the taken parts are valid ip-section
+	 * add the taken section to the currentIP and add an "." as well;
+	 * call checkIpSections() the check the next ip-section recursion; 
+	 * @param ip
+	 * @param count
+	 * @param currentIP
+	 * @param ipSets
+	 */
 	private static void checkIpSections(String ip, int count, String currentIP, ArrayList<String> ipSets) {
 		// TODO Auto-generated method stub
-	//	if(ip.length()>(count*3)) return;
-		
 		if(count==1 && validSection(ip)){
 			currentIP += ip;
 			ipSets.add(currentIP);
 			return;
 					
-		} else {
+		} else if(ip.length() <= count*3){
+			System.out.println(" count " + count);
+			
 			for(int i=1; i<4 && i<ip.length(); i++){
 				String nextSection = ip.substring(0, i);
 				
@@ -74,13 +93,22 @@ public class RestoreIPAddress {
 		
 	}//end checkIpSections() method
 
+	/********
+	 * to check if a string could be a valid section of ip address
+	 * ip arrange: [0-255];
+	 * 
+	 * @param ip
+	 * @return
+	 */
 	private static boolean validSection(String ip) {
 		// TODO check if the last few digits is a valid ip-section
 		if(ip.charAt(0) == '0') return ip.equals("0"); //this is very important, try ip=010010 :)
 				
 		int num = Integer.parseInt(ip); 
 		return num<=255 && num>=0;
-	}
+		
+	}//end of validSection() method;
+	
 
 	private static void printArrayList(ArrayList<String> str) {
 		// TODO printout every string in an arrayList
